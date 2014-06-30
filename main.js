9a4371254dcdd2d971f5ce94ceaf4e30f3b88621
Swaff <swaff68@gmail.com>
2014-06-30 15:50:21 -0600

    finished

--- main.js
+++ main.js
@@ -1,3 +1,43 @@
 $(document).on('ready', function() {
-  
+
+
+var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
+
+
+// Problem 1  
+
+for(var i =0; i<animals.length-1; i++){
+// 	console.log(animals[i]);
+}
+
+
+// Problem 2
+for(var i =0; i<animals.length; i+=2){
+	console.log(animals[i]);
+
+	}
+
+// Problem 3
+for(var i =animals.length-1; i>=0; i--){
+	console.log(animals[i]);
+
+	}
+
+
+// Problem 4
+
+for(var i =0; i<animals.length; i++){
+	if(i===0 || i=== animals.length-1){
+		console.log(animals[i]);
+	}
+
+	else {
+
+		console.log (animals[i]);
+		console.log (animals[i]);
+
+	}
+}
+
+
 });
